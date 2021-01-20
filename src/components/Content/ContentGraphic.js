0fa7450e756ac2a6e9d60d44e4057bfbbe253d30
contidominico <dominicoclt@gmx.co.uk>
2021-01-20 13:44:15 +0000

    my initial

---
+++ src/components/Content/ContentGraphic.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ContentGraphic() {
+	return (
+		<img src="https://i.ibb.co/pRfW0vT/leo.jpg" alt="leo" width = "100%" height="100%"></img>
+	)
+}
+
+export default ContentGraphic
